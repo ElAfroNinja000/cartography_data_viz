bef995d3feedfec62c554dc01be1b8545990ba2b
AfroNinjaaaaaaaa <m1105.108.megaman@gmail.com>
2023-01-19 21:18:58 +0100

    Refactory Added tiles and feature groups by countries

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
 import os
-import pandas as pd
 import folium
 import webbrowser
+import pandas as pd
 
 DATA_PATH = f"{os.getcwd()}/data/"
 
 
-def build_df_by_countries():
-    df_raw = pd.read_csv(f"{DATA_PATH}world_countries.csv")
-    df_countries_data = df_raw[["country", "population", "pop_density", "net_migration", "infant_mortality", "gdp",
-                                "literacy", "phones", "climate", "birth_rate", "death_rate", "agriculture", "industry",
-                                "service"]].fillna(0)
-    df_countries_coords = pd.read_csv(f"{DATA_PATH}countries_coords.csv")
+class WorldDataAnalytics:
+    def __init__(self, filename: str):
+        self.df = pd.read_csv(f"{DATA_PATH}{filename}")
+        center_point = self.df[["latitude", "longitude"]].mean().values.tolist()
+        south_west =   self.df[["latitude", "longitude"]].min().values.tolist()
+        north_east =   self.df[["latitude", "longitude"]].max().values.tolist()
+        tiles = ["cartodbpositron", "Stamen Terrain", "Stamen Toner"]
+        self.map_obj = folium.Map(location=center_point, tiles=tiles[0])
+        for tile in tiles:
+            folium.TileLayer(tile).add_to(self.map_obj)
+        self.map_obj.fit_bounds([south_west, north_east])
 
-    return pd.merge(left=df_countries_data, right=df_countries_coords, left_on="country", right_on="country")
+    def __display_map__(self, output_name: str):
+        folium.LayerControl().add_to(self.map_obj)
+        self.map_obj.save(f"{DATA_PATH}{output_name}")
+        webbrowser.open(f"{DATA_PATH}{output_name}")
 
+    def __country_data_to_popup__(self, country_name: str, country_metrics: dict):
+        popup_text = f"<b>{country_name}:</b>"
+        for key, value in country_metrics.items():
+            popup_text += f"<br>{key}: {str(value)}"
+        return folium.Popup(folium.IFrame(popup_text), min_width=150, max_width=150)
 
-def country_data_to_popup(country_name, infant_mortality, gdp):
-    return f""" {country_name}
-    Infant Mortality: {infant_mortality}
-    GDP: {gdp}
-    """
+    def compute_data(self, metrics):
+        for lat, lng, country_name in zip(self.df["latitude"], self.df["longitude"], self.df["country"]):
+            feature_group = folium.FeatureGroup(country_name)
+            country_metrics = dict(zip(metrics, [self.df[metric] for metric in metrics]))
+            folium.Marker([lat, lng], popup=self.__country_data_to_popup__(country_name, country_metrics),
+                          tooltip=f"{lat}, {lng}").add_to(feature_group)
+            feature_group.add_to(self.map_obj)
+        self.__display_map__("map.html")
 
 
 if __name__ == "__main__":
-    df = pd.read_csv(f"{DATA_PATH}data_by_countries.csv")
-
-    center_point = df[["latitude", "longitude"]].mean().values.tolist()
-    south_west = df[["latitude", "longitude"]].min().values.tolist()
-    north_east = df[["latitude", "longitude"]].max().values.tolist()
-
-    map = folium.Map(location=center_point, tiles="Stamen Toner")
-    for lat, lng, country, infant_mortality, gdp in zip(df["latitude"], df["longitude"], df["country"], df["infant_mortality"], df["gdp"]):
-        folium.Marker([lat, lng], popup=f"{country_data_to_popup(country, infant_mortality, gdp)}", tooltip=f"{lat}, {lng}").add_to(map)
-    map.fit_bounds([south_west, north_east])
-    map.save(f"{DATA_PATH}map.html")
-    webbrowser.open(f"{DATA_PATH}map.html")
+    metrics = ["population", "pop_density", "net_migration", "infant_mortality", "gdp",
+               "literacy", "phones", "climate", "birth_rate", "death_rate"]
+    wda = WorldDataAnalytics("data_by_countries.csv")
+    wda.compute_data(metrics)
